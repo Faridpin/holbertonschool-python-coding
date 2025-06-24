80d40e400906b32c71aa0c4a8a7c9ac7a35183c0
Faridpin <farid.mahmudlu.std@bhos.edu.az>
2025-06-24 10:10:57 +0000

    3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
-'''This module uses getter and setter'''
+""" This module defines a Square class with validation """
 
 
 class Square:
-    ''' Square class '''
+    """ Square class """
+
     def __init__(self, size=0):
-        self.__size = size
+        self.size = size  # Use the setter to ensure validation
 
     @property
     def size(self):
-        print(self.__size)
+        """Gets the current size of the square."""
+        return self.__size
 
     @size.setter
-    def size(self, size):
-        self.__size = size
+    def size(self, value):
+        """Sets the size of the square with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
+        """Calculates the area of the square."""
         return self.__size ** 2
